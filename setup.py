3d3e54a4c97dab5d67c11c0db07ddd46c50f0d25
gammaflauge <mddevine@gmail.com>
2020-02-24 21:50:21 -0500

    :tada: Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ml learning on https://archive.ics.uci.edu/ml/datasets/mushroom',
+    author='gammaflauge',
+    license='MIT',
+)
