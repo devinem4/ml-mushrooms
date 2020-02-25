df2b2af56f31e5ba3939d21986c874db73db2f13
gammaflauge <mddevine@gmail.com>
2020-02-24 22:42:00 -0500

    :wrench: Build data pipeline

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,75 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
+""" main data pipeline for transforming the wine dataset """
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from metaflow import FlowSpec, step, IncludeFile
+
+PROJECT_DIR = Path(__file__).resolve().parents[2]
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+class MushroomFlow(FlowSpec):
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    A flow to read in the mushroom dataset -- see src.data.download_raw.py
+    https://archive.ics.uci.edu/ml/datasets/mushrooms
+    """
+
+    raw_data_path = PROJECT_DIR / "data" / "raw" / "mushrooms.csv"
+    raw_mushroom_data = IncludeFile(
+        "mushroom_data",
+        help="Mushroom records drawn from The Audubon Society Field Guide to North American Mushrooms (1981).",
+        default=raw_data_path,
+    )
+
+    @step
+    def start(self):
+        """
+        Read data/raw/mushrooms.csv into a pandas df
+        """
+        import pandas as pd
+        from io import StringIO
+
+        self.dataframe = pd.read_csv(StringIO(self.raw_mushroom_data))
+        self.next(self.rename_cols)
+
+    @step
+    def rename_cols(self):
+        """
+        Give the mushroom data some computer friendlier column names
+        Save to "data/interim/mushroom_nice_cols.csv"
+        """
+        self.dataframe.columns = [
+            col.lower().replace(" ", "_") for col in self.dataframe.columns
+        ]
+
+        save_path = PROJECT_DIR / "data" / "interim" / "mushroom_nice_cols.csv"
+        self.dataframe.to_csv(save_path, index=False)
+
+        self.next(self.one_hot_encode)
+    
+    @step
+    def one_hot_encode(self):
+        """
+        Convert the categorical columns to one hot encoding.
+        We'll do it by hand this time.
+        """
+        # col 1 = class
+        for col in self.dataframe.columns[1:]:
+            possible_vals = self.dataframe[col].unique()
+            for v in possible_vals:
+                self.dataframe[f"{col}__{v}"] = self.dataframe[col].apply(lambda x: 1 if x == v else 0)
+            self.dataframe = self.dataframe.drop(col, axis="columns")
+
+        save_path = PROJECT_DIR / "data" / "interim" / "mushroom_one_hot.csv"
+        self.dataframe.to_csv(save_path, index=False)
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """
+        Save the final version to "data/processed/mushroom_final.csv"
+        End the flow.
+        """
+        save_path = PROJECT_DIR / "data" / "processed" / "mushroom_final.csv"
+        self.dataframe.to_csv(save_path, index=False)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+if __name__ == "__main__":
+    MushroomFlow()
